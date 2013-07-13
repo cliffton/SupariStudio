98ac8e801db050b2ee57c92c2a5198ca7ae13ecf
Cliffton <cliffton.fernandes.rulz@gmail.com>
2013-07-13 06:50:56 +0530

    Added The Flip
    +added nav bar
    +flip
    +additional files
    lots of bugs

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,28 @@
-$(document).ready(function()
-{
-	$('#entryVid').click(function()
-	{
-		$('.topbar').addClass("popCircles");
-	});
-});
+  $(document).ready(function(){
+            // set up hover panels
+            // although this can be done without JavaScript, we've attached these events
+            // because it causes the hover to be triggered when the element is tapped on a touch device
+            $('.item').hover(function(){
+                $(this).addClass('flip');
+            },function(){
+                $(this).removeClass('flip');
+            });
+
+            //move div yeah !!!
+            $('#goLeft').click(function()
+            {
+                $('.workContainer').animate({left:'-200px'});
+                $('#goLeft').animate({left:'200px'});
+            });
+
+             $('#goRight').click(function()
+            {
+                $('.workContainer').animate({left:'200px'});
+                $('#goRight').animate({left:'200px'});
+            });
+
+            $('#treasure').click(function()
+            {
+                
+            });
+        });
